4e76ac132e5fad98bc487d5f2e70994b0bb44044
Vincent Tang <tangtrinhquang@gmail.com>
2024-12-19 10:58:14 +0700

    update

---
+++ app/containers/Pages/Users/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import brand from 'dan-api/dummy/brand';
+import { LoginForm } from 'dan-components';
+import useStyles from 'dan-components/Forms/user-jss';
+
+function Login() {
+  const title = brand.name + ' - Login';
+  const description = brand.desc;
+  const { classes } = useStyles();
+  return (
+    <div className={classes.root}>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+      </Helmet>
+      <div className={classes.container}>
+        <div className={classes.userFormWrap}>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
